77f9e0364486ae26c121b966b9709bd22166ee39
Satklomi <2276316223@qq.com>
2026-03-26 01:28:57 +0800

    词法分析器完全完成

--- base/common.h
+++ base/common.h
@@ -7,5 +7,6 @@
 
 // 以下是用到的标准库函数列表。需要移植请按此实现。
 #define yaya_printf printf
+#define yaya_strcmp strcmp
 // operator new
 // operator delete
--- preprocessor/preprocessor.hpp
+++ preprocessor/preprocessor.hpp
@@ -4,12 +4,6 @@
 
 class Preprocessor {
 private:
-    bool str_eq(const char *a, const char *b, int len) {
-        for (int i = 0; i < len; i++) {
-            if (a[i] != b[i]) return false;
-        }
-        return true;
-    }
     bool is_keyword(Token &tok) {
         const char *keywords[] = {
             "if", "for", "while", "operator", "var", "val", "else", "from", "func",
@@ -18,7 +12,7 @@ private:
             "true", "false", "null"
         };
         for (int j = 0; j < sizeof(keywords) / sizeof(*keywords); j++) {
-            if (str_eq(tok.start, keywords[j], strlen(keywords[j]))) return true;
+            if (!yaya_strcmp(tok.start, keywords[j])) return true;
         }
         return false;
     }
